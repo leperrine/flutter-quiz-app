d4f72355a2f461be78f6082d7f8d11d6848b1e56
leperrine <leperrine@gmail.com>
2019-12-24 10:28:40 -0600

    first commit

---
+++ lib/result.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'You are pretty likable';
+    } else if (resultScore <= 16) {
+      resultText = 'You need to work on yourself!';
+    } else {
+      resultText = 'There is no hope for you';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
